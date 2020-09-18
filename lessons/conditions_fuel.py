e70c609177edb634817f1a1cd9baff49d8bdb9be
Qiufeng54321 <2710923955@qq.com>
2020-09-18 13:44:39 +0800

    🎈perf(lesson): remove condition

--- lessons/conditions_fuel.py
+++ lessons/conditions_fuel.py
@@ -1,5 +1,6 @@
+# Lalalalalalala I don't use condition statement yeahhhhhhhhhhhhhhhhhhhh
 coefficients: tuple = (.176, .236, .208, .296)
-fuel_type: int = 2 if input("Fuel type: ") == "petrol" else 0
-volume: int = 1 if float(input("Input the volume: ")) > 2 else 0
+fuel_type: int = int(input("Fuel type: ") == "petrol") * 2
+volume: int = int(float(input("Input the volume: ")) > 2)
 distance: float = float(input("Distance: "))
 print("Emmision is", coefficients[fuel_type + volume] * distance / 1000)
